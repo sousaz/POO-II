86d169a3e421a1cff5cfc6450c8c19b688baaf4d
sousaz <gabrielsousa2003gsa.gmail.com>
2024-03-13 14:58:00 -0300

    atividade 2 pronta

--- FolhaDePagamento/src/Main.java
+++ FolhaDePagamento/src/Main.java
@@ -2,9 +2,13 @@ import atividade01.Coletavel;
 import atividade01.Espada;
 import atividade01.Heroi;
 import atividade01.Monstro;
+import atv_comparable.Agenda;
+import atv_comparable.Pessoa;
 import folhaPagamento.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -21,17 +25,31 @@ public class Main {
 //        FolhaPagamento fp = new FolhaPagamento(funcs);
 //        System.out.println("Total da folha: " + fp.gerarFolha());
 
-        Espada espada1 = new Espada("tomaaaa", 30);
-        Heroi h1 = new Heroi("jarvis", 10, 100, 5);
-        Monstro m1 = new Monstro("Zumbi", 40, 200, 50);
+//        Espada espada1 = new Espada("tomaaaa", 30);
+//        Heroi h1 = new Heroi("jarvis", 10, 100, 5);
+//        Monstro m1 = new Monstro("Zumbi", 40, 200, 50);
+//
+//        h1.pegarItem(espada1);
+//        h1.ataqueEspecial();
+//        m1.atacar();
+//        h1.atacar();
+//        h1.curar();
+//        m1.defender();
+//
+//        System.out.println(h1.atacar());
 
-        h1.pegarItem(espada1);
-        h1.ataqueEspecial();
-        m1.atacar();
-        h1.atacar();
-        h1.curar();
-        m1.defender();
 
-        System.out.println(h1.atacar());
+        Agenda agenda = new Agenda();
+        agenda.setAgenda(new Pessoa("gabriel", "silva", LocalDate.of(1790, 01, 01), "1111111111"));
+        agenda.setAgenda(new Pessoa("gabriel", "augusto", LocalDate.of(1990, 01, 01), "1111111111"));
+        agenda.setAgenda(new Pessoa("augusto", "silva", LocalDate.of(1990, 01, 01), "1111111111"));
+        agenda.setAgenda(new Pessoa("augusto", "gabriel", LocalDate.of(1990, 01, 01), "1111111111"));
+        for (Pessoa pessoa: agenda.getAgenda()) {
+            System.out.println(pessoa.getNome() + " " + pessoa.getSobrenome() + " " + pessoa.getDataNascimento());
+        }
+        System.out.println();
+        for (Pessoa pessoa: agenda.ordenarAgenda()) {
+            System.out.println(pessoa.getNome() + " " + pessoa.getSobrenome() + " " + pessoa.getDataNascimento());
+        }
     }
 }
